8c0a395881475447393a91adb490ba3218d8370d
p-anja <anja.pesic@positivetech.rs>
2023-02-06 13:12:14 +0100

    feat: modal forms validation

--- moja-kuhinjica-frontend/src/components/modal/signUp/SignUpModal.tsx
+++ moja-kuhinjica-frontend/src/components/modal/signUp/SignUpModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Modal from 'react-modal'
 import Image from 'next/image'
+import { useForm } from 'react-hook-form'
 import { FormInput } from '@/components/input/FormInput'
 import styles from './SignUpModal.module.scss'
 import { bgModal } from '@/constants/constants'
@@ -16,6 +17,15 @@ interface ISignUpModalProps {
     closeModal: (param: boolean) => void
 }
 export const SignUpModal = ({ modalIsOpen, closeModal }: ISignUpModalProps) => {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm()
+    const onSubmit = (data: any) => {
+        reset()
+    }
     return (
         <Modal
             isOpen={modalIsOpen}
@@ -25,23 +35,70 @@ export const SignUpModal = ({ modalIsOpen, closeModal }: ISignUpModalProps) => {
             ariaHideApp={false}
         >
             <div className={styles.formContainer}>
-                <div className={styles.formDiv}>
+                <form
+                    className={styles.formDiv}
+                    onSubmit={handleSubmit(onSubmit)}
+                >
                     <label className={styles.formTitle}>Registrujte se</label>
                     <FormInput
+                        register={register}
+                        errors={errors}
+                        name="name"
                         src={profile}
                         placeholder="Ime i prezime"
                         type="text"
+                        validationSchema={{
+                            required: 'name is required',
+                            pattern: {
+                                value: /[A-Za-z]/,
+                                message: 'invalid name value',
+                            },
+                        }}
                     />
-                    <FormInput src={email} placeholder="Email" type="text" />
                     <FormInput
+                        register={register}
+                        errors={errors}
+                        name="email"
+                        src={email}
+                        placeholder="Email"
+                        type="text"
+                        validationSchema={{
+                            required: 'email is required',
+                            pattern: {
+                                value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                                message: 'invalid email value',
+                            },
+                        }}
+                    />
+                    <FormInput
+                        register={register}
+                        errors={errors}
+                        name="password"
                         src={password}
                         placeholder="Šifra"
                         type="password"
+                        validationSchema={{
+                            required: 'password is required',
+                            pattern: {
+                                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                                message: 'invalid password value',
+                            },
+                        }}
                     />
                     <FormInput
+                        register={register}
+                        errors={errors}
+                        name="telephoneNumber"
                         src={mobile}
                         placeholder="Broj telefona"
                         type="text"
+                        validationSchema={{
+                            required: 'telephone number is required',
+                            pattern: {
+                                value: /^[0-9]{6,}$/,
+                                message: 'invalid telephone number value',
+                            },
+                        }}
                     />
                     <button className={styles.formButton}>Potvrdi</button>
                     <div className={styles.separatorWrapper}>
@@ -53,7 +110,7 @@ export const SignUpModal = ({ modalIsOpen, closeModal }: ISignUpModalProps) => {
                         <Image src={google} alt="" />
                         <Image src={mailSignUp} alt="" />
                     </div>
-                </div>
+                </form>
             </div>
         </Modal>
     )
