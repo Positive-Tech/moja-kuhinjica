cbdfad823c4f63076d747b063911ca81da07436d
p-anja <anja.pesic@positivetech.rs>
2023-02-28 10:22:36 +0100

    feat: change password mobile page

--- moja-kuhinjica-frontend/src/pages/passwordChange/index.tsx
+++ moja-kuhinjica-frontend/src/pages/passwordChange/index.tsx
@@ -1,8 +1,103 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import { FieldValues, useForm } from 'react-hook-form'
+import React, { useState } from 'react'
 import styles from './ChangePasswordPage.module.scss'
+import passwordIcon from 'public/static/assets/images/password.svg'
+import { FormInput } from '@/components/input/FormInput'
+import { ErrorLabel } from '@/components/label/ErrorLabel'
+import { MobileHeader } from '@/components/header/mobileHeader/MobileHeader'
+import Menu from '@/components/mobileMenu'
+import { MobileFooter } from '@/components/footer/mobileFooter/MobileFooter'
 
-const ChangePasswordPage = () => {
-    return <div>ChangePasswordPage</div>
+const ChangePasswordPage = (): JSX.Element => {
+    const [showError, setShowError] = useState<boolean>(false)
+    const [errorMessage, setErrorMessage] = useState<string>('')
+    const [showMenu, setShowMenu] = useState<boolean>(false)
+    const router = useRouter()
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm()
+
+    const validate = (data: FieldValues): void => {
+        if (data.password === data.confirmPassword) {
+            delete data.confirmPassword
+            data.phoneNumber = `+381${data.phoneNumber}`
+            setShowError(false)
+            // changePassword(data)
+        } else {
+            setErrorMessage('Šifre se ne poklapaju. Pokušajte ponovo.')
+            setShowError(true)
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            {showMenu && <Menu closeMenu={() => setShowMenu(false)} />}
+            <MobileHeader handleClick={() => setShowMenu(true)} />
+            <div className={styles.wrapper}>
+                <form
+                    className={styles.formDiv}
+                    onSubmit={handleSubmit((data) => validate(data))}
+                >
+                    <label className={styles.formTitle}>Promeni šifru</label>
+                    {showError && <ErrorLabel content={errorMessage} />}
+                    <FormInput
+                        register={register}
+                        errors={errors}
+                        name="oldPassword"
+                        src={passwordIcon}
+                        placeholder="Stara šifra"
+                        type="password"
+                        validationSchema={{
+                            required: 'password is required',
+                            pattern: {
+                                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                                message: 'invalid password value',
+                            },
+                        }}
+                        style={styles.input}
+                    />
+                    <FormInput
+                        register={register}
+                        errors={errors}
+                        name="newPassword"
+                        src={passwordIcon}
+                        placeholder="Nova šifra"
+                        type="password"
+                        validationSchema={{
+                            required: 'password is required',
+                            pattern: {
+                                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                                message: 'invalid password value',
+                            },
+                        }}
+                        style={styles.input}
+                    />
+                    <FormInput
+                        register={register}
+                        errors={errors}
+                        name="confirmPassword"
+                        src={passwordIcon}
+                        placeholder="Potvrdi novu šifru"
+                        type="password"
+                        validationSchema={{
+                            required: 'password is required',
+                            pattern: {
+                                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                                message: 'invalid password value',
+                            },
+                        }}
+                        style={styles.input}
+                    />
+                    <button className={styles.formButton}>Potvrdi</button>
+                </form>
+            </div>
+            <MobileFooter />
+        </div>
+    )
 }
 
 export default ChangePasswordPage
