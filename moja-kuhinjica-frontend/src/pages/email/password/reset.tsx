891f8d3a1743f3d9677c54d20d2ca6a11b8d3b42
p-anja <anja.pesic@positivetech.rs>
2023-03-03 16:20:43 +0100

    feat: reset password from email

--- moja-kuhinjica-frontend/src/pages/email/password/reset.tsx
+++ moja-kuhinjica-frontend/src/pages/email/password/reset.tsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from '../EmailPages.module.scss'
+import passwordIcon from 'public/static/assets/images/password.svg'
+import { Title } from '@/components/label/Title'
+import { useRouter } from 'next/router'
+import { FormInput } from '@/components/input/FormInput'
+import { FieldValues, useForm } from 'react-hook-form'
+import { ErrorLabel } from '@/components/label/ErrorLabel'
 
-const ResetPasswordPage = () => {
-    return <div>verification</div>
+const ResetPasswordPage = (): JSX.Element => {
+    const [showError, setShowError] = useState<boolean>(false)
+    const [errorMessage, setErrorMessage] = useState<string>('')
+    const router = useRouter()
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm()
+
+    const validate = (data: FieldValues): void => {
+        if (data.newPassword === data.confirmPassword) {
+        } else {
+            setErrorMessage('Šifre se ne poklapaju. Pokušajte ponovo.')
+            setShowError(true)
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.resetPassWrapper}>
+                <Title
+                    content="Resetovanje šifre"
+                    style={styles.resetPassTitle}
+                />
+                <div className={styles.formWrapper}>
+                    <form
+                        onSubmit={handleSubmit((data) => validate(data))}
+                        className={styles.formDiv}
+                    >
+                        {showError && <ErrorLabel content={errorMessage} />}
+                        <FormInput
+                            register={register}
+                            errors={errors}
+                            name="newPassword"
+                            src={passwordIcon}
+                            placeholder="Unesi novu šifru"
+                            type="password"
+                            validationSchema={{
+                                required: 'password is required',
+                                pattern: {
+                                    value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                                    message: 'invalid password value',
+                                },
+                            }}
+                            style={styles.passwordInput}
+                        />
+                        <FormInput
+                            register={register}
+                            errors={errors}
+                            name="confirmPassword"
+                            src={passwordIcon}
+                            placeholder="Potvrdi novu šifru"
+                            type="password"
+                            validationSchema={{
+                                required: 'pass is required',
+                            }}
+                            style={styles.passwordInput}
+                        />
+                        <button type="submit" className={styles.formButton}>
+                            Resetuj šifru
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
-
 export default ResetPasswordPage
